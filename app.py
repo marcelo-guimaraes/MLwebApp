ad90601e59263edc7af0a0822cd245ab5f58e16d
Marcelo <marcelofelippe.mfg@gmail.com>
2020-06-27 14:17:53 -0300

    Upload CSV file
    This code already upload a csv file and also gives the option to choose is your ML problem
    is a Regression or a Classification problem

--- app.py
+++ app.py
@@ -2,13 +2,19 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplot.pyplot as plt
+import matplotlib.pyplot as plt
 
 def main():
-    st.title("Hello World")
-    st.header("This is a Header")
-    st.subheader("This is a subheader")
-    st.text("This is a text")
+    st.title("Machine Learning web App")
+    st.header("This WebApp was created for solve your Machine Learning problem")
+    st.subheader("Import your csv file")
+    st.file_uploader("Choose your file", type = 'csv')
+
+    radio = st.sidebar.radio('Select the type of your problem',("Regression", "Classification"))
+    if radio == "Regression":
+        st.markdown("Reg problem")
+    if radio == "Classification":
+        st.markdown("Classification problem")
 
 
 if __name__ == '__main__':
