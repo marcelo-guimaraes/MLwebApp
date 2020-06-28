d4be88ac6b6e19dbfdca1443b5b8b5dbb28d69dd
Marcelo <marcelofelippe.mfg@gmail.com>
2020-06-28 02:11:52 -0300

    Fixing import file func

--- app.py
+++ app.py
@@ -9,9 +9,20 @@ def load_data(csv_file):
     df = pd.read_csv(csv_file)
     return df
 
-def print_missing_values(dataframe):
-    df = pd.DataFrame({'Missing': dataframe.isna().sum()})
-    st.write(df)
+@st.cache(persist = True)
+def print_stats(data):
+    df = pd.DataFrame({'Missing': data.isna().sum(),
+                       'Cardinality': data.nunique(),
+                       'Type': data.dtypes})
+    return df.sort_values('Type')
+
+def input_values(data):
+    obj_cols = data.select_dtypes('object').to_list()
+    num_cols = data.select_dtypes(exclude=['object']).to_list()
+    data[obj_cols] = data[obj_cols].fillna(data[obj_cols].mode().iloc[0])
+    data[num_cols] = data[num_cols].fillna(data[num_cols].mode().iloc[0])
+
+    return data
 
 def main():
     st.title("Machine Learning web App")
@@ -21,13 +32,15 @@ def main():
     file = st.file_uploader("Choose your file", type = 'csv')
     if file is not None:
         df = load_data(file)
-
-    st.write(df)
-
-    st.header("Data Cleansing and Basic Statistics of your Data")
-    stats = st.selectbox("Select the  Statitics", ("% Missing Values", "Type", "Cardinality"))
-    if stats == "% Missing Values":
-        print_missing_values(df)
+        st.write(df)
+        st.header("Data Cleansing and Basic Statistics of your Data")
+        st.write(print_stats(df))
+        if df.isnull().sum().sum() > 0:
+            st.subheader("The Dataset have Missing Values. Presse the button below to handle with them")
+            missing = st.button('Input values based on the mode')
+            if missing:
+                df = input_values(df)
+                st.markdown("Done!!")
 
 
 
